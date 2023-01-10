77f6b9924fbe8b72f0f9408348bc8546fa1a09ee
the-groot <sangwoo0795@naver.com>
2023-01-10 21:49:27 +0900

    d

--- auth/src/main/java/com/example/auth/Service/CustomUserDetailsService.java
+++ auth/src/main/java/com/example/auth/Service/CustomUserDetailsService.java
@@ -8,12 +8,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
