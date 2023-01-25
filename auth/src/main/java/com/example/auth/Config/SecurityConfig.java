ec5165ff82e944de073c2dccb6977f8f6415a883
the-groot <sangwoo0795@naver.com>
2023-01-25 10:35:47 +0900

    네이버, 구글, 카카오 로그인 되는 것은 확인

--- auth/src/main/java/com/example/auth/Config/SecurityConfig.java
+++ auth/src/main/java/com/example/auth/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.auth.Config;
 
 import com.example.auth.Oauth2.CustomOAuth2AuthService;
+import com.example.auth.Oauth2.CustomOidcUserService;
 import com.example.auth.Oauth2.OAuth2AuthenticationFailureHandler;
 import com.example.auth.Oauth2.OAuth2AuthenticationSuccessHandler;
 import com.example.auth.Security.*;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,19 +42,21 @@ public class SecurityConfig {
     private final CustomOAuth2AuthService customOAuth2AuthService;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-
+    private final CustomOidcUserService customOidcUserService;
 
     public SecurityConfig(JwtAccessDeniedHandler jwtAccessDeniedHandler,
             JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
             TokenProvider tokenProvider, CustomOAuth2AuthService customOAuth2AuthService,
             OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler,
-            OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
+            OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler,
+            CustomOidcUserService customOidcUserService) {
         this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
         this.tokenProvider = tokenProvider;
         this.customOAuth2AuthService = customOAuth2AuthService;
         this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
         this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+        this.customOidcUserService=customOidcUserService;
     }
 
 
@@ -64,13 +68,14 @@ public class SecurityConfig {
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http
+//
 //                .csrf().disable()
 //                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //                .and()
 //
 //                .exceptionHandling()
 //                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
+//               .accessDeniedHandler(jwtAccessDeniedHandler)
 //                .and()
 //
 //                .authorizeRequests()
@@ -106,13 +111,21 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOAuth2AuthService)
+                .authorizeRequests().anyRequest().permitAll()
                 .and()
-                .successHandler(oAuth2AuthenticationSuccessHandler)
-                .failureHandler(oAuth2AuthenticationFailureHandler);
+                .oauth2Login();
+            //    .userInfoEndpoint()
+             //   .oidcUserService(customOidcUserService)
+              //  .userService(customOAuth2AuthService)
+               // .and()
+                //.successHandler(oAuth2AuthenticationSuccessHandler)
+                //.failureHandler(oAuth2AuthenticationFailureHandler);
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**"));
     }
 
 }
